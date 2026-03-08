b14d44b72256caaefb86ca6a3544122d697658a9
fullstack290623 <fullstack290623@gmail.com>
2026-03-08 19:19:25 +0200

    upload

---
+++ 01_tuple.py
@@ -0,0 +1,107 @@
+
+# bool
+# int
+# float
+# str
+# list
+# set
+# fun
+
+# tuple -- frozen -- immutable
+number = (10,)
+print(number)
+
+numbers = (10, 20, 30)
+print(numbers)
+
+fruits = ("apple", "banana", "orange")
+print(fruits)
+
+scores = (100, 95, 87)
+
+# scores.sort()  # ERROR
+print(tuple(sorted(scores)))
+
+person = ("Alice", 25, "Teacher")
+print(person)
+
+animals = "dog", "cat", "lion"
+animals = ("dog", "cat", "lion")
+
+print(animals[0])
+print(animals[1])
+
+a=1
+b=2
+a, b = b, a
+print(a, b)
+
+colors = ("red", "green", "blue", "yellow")
+print(colors[-1])
+print(colors[::-1])
+print(colors[::2])
+
+flags = (False, False, True)
+print(any(flags))
+numbers = (1, 7, 5, 11, 3, 6, -1)
+print(any(x % 2 == 0 for x in numbers))
+print(all(x % 2 == 0 for x in numbers))
+print(all(len(color) >= 3 for color in colors))
+
+cars = ("Tesla", "Tesla", "Mitsubishi", "Honda")
+print(len(cars))
+
+animals = ("dog", "cat", "lion")
+for animal in animals:
+    print(animal)
+for i in range(len(animals)):
+    print(animals[i])
+print(len(animals[0]))
+
+print(cars)
+print([len(word) for word in cars])  # comprehension
+# cars[0] = 'new'  # Error
+# cars.add('fiat')
+
+numbers = (1, 2, 3, 2, 2, 5)
+print(numbers.count(2))
+
+l1 = []
+for x in range(1, 100 + 1):
+    l1.append(x)
+tuple_1_100 = tuple(l1)
+
+#          0  1  2  3  4   5    6
+numbers = (5, 8, 3, 9, 3, 99, 100)
+print(numbers.index(3, 3, 5))
+
+numbers = (5, 10, 15, 30)
+print(max(numbers))
+
+numbers = (5, 10, 15)
+print(min(numbers))
+
+numbers = (9, 3, 7, 1)
+print(sorted(numbers))
+
+days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+print(days)
+
+t1 = (10, [1,2,3], 'hi', {1, 2}, (3, 4), True)
+print(t1)
+print([1,2,3] in t1)
+
+# 1 create tuple with 3 of your favorite movies
+# 2 create tuple with all countries in Europe (ask ChatGPT)
+# 3 create tuple with all the days of feb (1, 2 ... 28)
+# 4 create tuple with all the days of dec (1, 2 .. 31)
+# 5 create tuple of all the month in the year
+# 6 create tuple of all USA presidents till today (ask ChatGPT), use len to find out how many are they
+# 7 use mean to find the avg of this tuple = (8, 11, -3, 12)
+
+
+
+
+
+
+
